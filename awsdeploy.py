35f9ac1d04bf6327611b56250a7c49c9e6b5ceb7
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2020-03-31 05:44:15 +0300

    Update awsdeploy.py

--- awsdeploy.py
+++ awsdeploy.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
-#job1
+######################Aws Script that deploy,stop,reboot, and terminate machines####################
+
 
 import boto3
 from time import sleep
@@ -16,7 +17,7 @@ def deploy(id,num,type,key):
     InstanceType=type,
     KeyName=key,
     GroupName='ssh')
-    print('New instanceID IS : ' + instance[0].id)
+    print('New instanceID IS >>: ' + instance[0].id)
 
 
 def stop():
@@ -24,13 +25,16 @@ def stop():
     ids = [id]
     ec2 = boto3.resource('ec2')
     ec2.instances.filter(InstanceIds = ids).stop()
-
+    sleep(0.5)
+    print("Stop successfully >>:", id)
 
 def start():
     id = input('Enter id that you want to start : ')
     ids = [id]
     ec2 = boto3.resource('ec2')
     ec2.instances.filter(InstanceIds = ids).start()
+    sleep(0.5)
+    print("Start successfully >>:", id)
 
 
 def reboot():
@@ -38,33 +42,19 @@ def reboot():
     ids = [id]
     ec2 = boto3.resource('ec2')
     ec2.instances.filter(InstanceIds = ids).reboot()
-
+    sleep(0.5)
+    print("Reboot successfully >>:", id)
+    
 
 def terminate():
     id = input('Enter the ID that you want to terminate : ')
     ids = [id]
     ec2 = boto3.resource('ec2')
     ec2.instances.filter(InstanceIds = ids).terminate()
+    sleep(0.5)
+    print("Terminate successfully >>:", id)
 
-def go_back():
-    a = 0
-    while a == 0:
-        back = input("Do you want to go back to the main menu?\n")
-        if back == "yes" or back == "Yes" or back == "YES":
-            print("Going back...")
-            sleep(1)
-            a = 1
-            acc = 0
-        elif back == "no" or back == "No" or back == "NO":
-            print("that`s ok!")
-            a = 1
-            acc = 1
-            account = 1
-        else:
-            print("Please choose yes or no")
-            sleep(1)
-
-
+    
 def show():
     ec2 = boto3.resource('ec2')
     for instance in ec2.instances.all():
@@ -76,10 +66,9 @@ def show():
             )
         )
 
-
-
+        
 def get_key():
-    print("These are the keys :")
+    print("These are the keys >>:")
     ec2 = boto3.client('ec2')
     response = ec2.describe_key_pairs()
     for i in response['KeyPairs']:
@@ -87,12 +76,11 @@ def get_key():
     return(response)
 
 
-
 def regions_name():
     client = boto3.client('ec2')
     regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
     n = 0
-    print("All regions available: \n")
+    print("All regions available >>:\n")
     for i in regions:
         n = n + 1
         print(str(n) + ". " + i)
@@ -112,23 +100,21 @@ def get_services(aws_access_key,aws_secret_key,region_name):
         n = n + 1
         print(str(n) + ". " + i)
 
-
-               ######### Menu #########
-
+           ######### Menu #########
 
 while True:
     print("------------------------")
-    print("1.all AWS services from your account\n2.Deploy machines\n3.stop Instances\n4.start Instances\n5.reboot Instances\n6.Terminate Instances\n7.Show instances\n8.Show keys\n9.Show regions\n10.Exit")
+    print("(1)>>all AWS services from your account\n(2)>>Deploy machines\n(3)>>stop Instances\n(4)>>start Instances\n(5)>>reboot Instances\n(6)>>Terminate Instances\n(7)>>Show instances\n(8)>>Show keys\n(9)>>Show regions\n(10)>>Exit")
     print("------------------------")
     sleep(1)
     choose = input("Enter 1 - 10 Pls :")
     if choose == "1":
-        get_services(input("Enter aws_access_key:\n"),input("Enter aws_secret_key:\n"),input("Enter region name:\n"))
+        get_services(input("Enter aws_access_key >>:\n"),input("Enter aws_secret_key >>:\n"),input("Enter region name >>:\n"))
 
 
     elif choose == "2":
         get_key()
-        deploy(input("Enter AMI id: "),int(input("Enter how many machines: ")),input("Enter type: "),input("Key type :"))
+        deploy(input("Enter AMI id >>:"),int(input("Enter how many machines >>:")),input("Enter type >>:"),input("Key type >>:"))
 
 
     elif choose == "3":
@@ -168,5 +154,5 @@ while True:
         break
 
     else:
-        print ("enter 1-10 only")
-
+        print ("<< Enter 1-10 only ! >>")
+####EOF####
